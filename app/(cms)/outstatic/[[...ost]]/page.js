a27acbae1bf628cc92218700d8e9dda2c9a97ed6
wasabi-atm <atmanawiera@gmail.com>
2026-07-19 19:13:12 +0800

    perf: fix Outstatic build slowness and optimize dynamic route rendering

--- app/(cms)/outstatic/[[...ost]]/page.js
+++ app/(cms)/outstatic/[[...ost]]/page.js
@@ -1,6 +1,7 @@
-import "outstatic/outstatic.css";
 import { Outstatic } from "outstatic";
 import { OstClient } from "outstatic/client";
+
+export const dynamic = 'force-dynamic';
 
 export default async function Page({ params }) {
   const ostData = await Outstatic();
--- app/api/outstatic/[[...ost]]/route.js
+++ app/api/outstatic/[[...ost]]/route.js
@@ -2,3 +2,5 @@ import { OutstaticApi } from "outstatic";
 
 export const GET = OutstaticApi.GET;
 export const POST = OutstaticApi.POST;
+
+export const dynamic = 'force-dynamic';
